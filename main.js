66b9beb5a6547c24a4b0abb09eb561562021eff1
Reinaldo <reinaldo18junior@gmail.com>
2024-04-25 16:03:46 -0300

    Exercício Concluído

---
+++ main.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('form-cadastro');
+const nomes = [];
+const telefones = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNome = document.getElementById('nome');
+    const inputTelefone = document.getElementById('telefone');
+
+    if (telefones.includes(inputTelefone.value)) {
+        alert(`O número de Telefone: ${inputTelefone.value} já foi inserido.`)
+    } else {
+        nomes.push(inputNome.value);
+        telefones.push(parseInt(inputTelefone.value));
+
+        let linha = '<tr>';
+        linha += `<td>${inputNome.value}</td>`;
+        linha += `<td>${inputTelefone.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+
+    inputNome.value = '';
+    inputTelefone.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
